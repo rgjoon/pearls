db559e6b22e17fec368dcd47d36ff1fbfb421ce6
Shinjoon Kang <rgjoon.dev@gmail.com>
2014-04-16 18:34:22 +0900

    add ex_01_01

---
+++ ex_01_01/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 10000000
+
+static int g_room[MAX_SIZE / 32] = {0, };
+
+void set(int n)
+{
+    g_room[n >> 5] |= 1 << (n & 0x1F);
+}
+
+int check(int n)
+{
+    return g_room[n >> 5] & (1 << (n & 0x1F));
+}
+
+int main()
+{
+    int i;
+    FILE * fp = 0;
+    
+    fp = fopen("data.txt", "r");
+    if( fp )
+    {
+        while( !feof(fp) )
+        {
+            char line[8] = {0, };
+            fread(line, 8, 1, fp);
+            
+            set(atoi(line));
+        }
+        
+        fclose(fp);
+    }
+    
+    printf("sorting has been complated.\n");
+    
+    fp = fopen("result.txt", "w+" );
+    if( fp )
+    {
+        for( i = 0; i < MAX_SIZE; i++ )
+        {
+            if( check(i) )
+            {
+                fprintf(fp, "%08d\n", i);
+            }
+        }
+        
+        fclose(fp);
+    }
+    
+    printf("done.\n");
+    
+    return 0;
+}
+
